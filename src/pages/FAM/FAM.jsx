59cb54227100c288e0f816145d31218863146873
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2023-09-07 20:27:27 +0530

    Added image hotspot
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/pages/FAM/FAM.css
    	modified:   src/pages/FAM/FAM.jsx

--- src/pages/FAM/FAM.jsx
+++ src/pages/FAM/FAM.jsx
@@ -1,9 +1,24 @@
-import { Theme, Flex, Heading, IconButton } from "@radix-ui/themes";
+import {
+  Theme,
+  Flex,
+  Heading,
+  IconButton,
+  Text,
+  Container,
+  Card,
+  Inset,
+  AspectRatio,
+  HoverCard,
+  Box,
+  Popover,
+  ScrollArea,
+} from "@radix-ui/themes";
 import { useState, useEffect } from "react";
-import { MoonStars, Sun } from "@phosphor-icons/react";
+import { Car, Plus, MoonStars, Sun } from "@phosphor-icons/react";
 import { SiteMenuDropdown } from "../../components/Dropdown/SiteMenuDropdown";
 import Footer from "../../components/Footer/Footer";
 import "./FAM.css";
+import LoremIpsum from "react-lorem-ipsum";
 
 export default function FAM() {
   // Dark Mode Setup
@@ -60,13 +75,77 @@ function FAMBody() {
   return (
     <>
       <Flex p={"3"} justify={"center"} style={{ minHeight: "100vh" }}>
-        <Flex height={"100%"} style={{outline: "1px solid red"}}>
+        <Flex
+          height={"100%"}
+          direction={"column"}
+          // style={{ outline: "1px solid red" }}
+        >
           <Heading align={"center"} size="9">
             FAM
           </Heading>
+          <Flex direction={"column"} gap={"3"} p={"4"}>
+            <Text size={"4"}>
+              <LoremIpsum p={"6"} />
+            </Text>
+          </Flex>
+          <Container size={"4"}>
+            <Flex position={"relative"}>
+              <AspectRatio ratio={4 / 3}>
+                <img
+                  src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
+                  style={{
+                    objectFit: "cover",
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "0.5rem",
+                  }}
+                />
+              </AspectRatio>
+              <ImageHotspot
+                top="30%"
+                left="20%"
+                title={"Hello!"}
+                content={<LoremIpsum p={3} />}
+              />
+              <ImageHotspot
+                top="10%"
+                left="20%"
+                title={"Hello!"}
+                content={<LoremIpsum p={3} />}
+              />
+            </Flex>
+          </Container>
         </Flex>
       </Flex>
+
       <Footer />
     </>
+  );
+}
+
+function ImageHotspot({ top, left, title, content }) {
+  return (
+    <Popover.Root>
+      <Popover.Trigger style={{ position: "absolute", top, left }}>
+        <IconButton color="bronze" radius="full" size={"2"} variant="solid">
+          <Plus size={"1rem"} weight="bold" />
+        </IconButton>
+      </Popover.Trigger>
+      <Popover.Content size="3" className="hotspot-content">
+        <Flex direction={"column"}>
+          <Heading>{title}</Heading>
+          <ScrollArea
+            size={"1"}
+            type="always"
+            scrollbars="vertical"
+            style={{ height: "fit-content", maxHeight: "20vh" }}
+          >
+            <Box pr={"3"}>
+              <Text size={"3"}>{content}</Text>
+            </Box>
+          </ScrollArea>
+        </Flex>
+      </Popover.Content>
+    </Popover.Root>
   );
 }
