bf67c3a2f963595981e44f7c645ca35b523d697f
Aniruddh Ravipati <ravipatianirudh@gmail.com>
2023-10-28 17:46:00 +0530

    Added some icons

--- src/components/SiteMenuDropdown/SiteMenuDropdown.jsx
+++ src/components/SiteMenuDropdown/SiteMenuDropdown.jsx
@@ -1,5 +1,7 @@
-import { List } from "@phosphor-icons/react";
-import { DropdownMenu, IconButton } from "@radix-ui/themes";
+import { Code, List, SealQuestion } from "@phosphor-icons/react";
+import { CubeFocus, HouseSimple, Info } from "@phosphor-icons/react/dist/ssr";
+import { Component1Icon } from "@radix-ui/react-icons";
+import { DropdownMenu, Flex, IconButton } from "@radix-ui/themes";
 import { Link as RouterLink } from "react-router-dom";
 
 export function SiteMenuDropdown() {
@@ -15,14 +17,23 @@ export function SiteMenuDropdown() {
       </DropdownMenu.Trigger>
       <DropdownMenu.Content variant="soft">
         <DropdownMenu.Item asChild>
-          <RouterLink to={"/"}> Home</RouterLink>
+          <RouterLink to={"/"}>
+            {" "}
+            <Flex align={"center"} gap={"2"}>
+              <HouseSimple size="1rem" />
+              Home
+            </Flex>
+          </RouterLink>
         </DropdownMenu.Item>
 
         <DropdownMenu.Separator />
 
         <DropdownMenu.Sub>
           <DropdownMenu.SubTrigger>
-            Digital Product Design
+            <Flex align={"center"} gap={"2"}>
+              <Component1Icon size="1rem" />
+              Digital Product Design
+            </Flex>
           </DropdownMenu.SubTrigger>
           <DropdownMenu.SubContent>
             <DropdownMenu.Item asChild>
@@ -32,7 +43,12 @@ export function SiteMenuDropdown() {
         </DropdownMenu.Sub>
 
         <DropdownMenu.Sub>
-          <DropdownMenu.SubTrigger>User Research</DropdownMenu.SubTrigger>
+          <DropdownMenu.SubTrigger>
+            <Flex align={"center"} gap={"2"}>
+              <SealQuestion size={"1rem"} />
+              User Research
+            </Flex>
+          </DropdownMenu.SubTrigger>
           <DropdownMenu.SubContent>
             <DropdownMenu.Item asChild>
               <RouterLink to={"/FAM"}> FAM</RouterLink>
@@ -42,7 +58,12 @@ export function SiteMenuDropdown() {
         </DropdownMenu.Sub>
 
         <DropdownMenu.Sub>
-          <DropdownMenu.SubTrigger>Creative Tech</DropdownMenu.SubTrigger>
+          <DropdownMenu.SubTrigger>
+            <Flex align={"center"} gap={"2"}>
+              <CubeFocus size={"1rem"} />
+              Creative Tech
+            </Flex>
+          </DropdownMenu.SubTrigger>
           <DropdownMenu.SubContent>
             <DropdownMenu.Item asChild>
               <RouterLink to={"/Shunya"}> Shunya</RouterLink>
@@ -58,15 +79,22 @@ export function SiteMenuDropdown() {
 
         <DropdownMenu.Item asChild>
           <RouterLink to={"/SoftwareEngineering"}>
-            {" "}
-            Software Engineering
+            <Flex align={"center"} gap={"2"}>
+              <Code size={"1rem"} />
+              Software Engineering
+            </Flex>
           </RouterLink>
         </DropdownMenu.Item>
 
         <DropdownMenu.Separator />
 
         <DropdownMenu.Item asChild>
-          <RouterLink to={"/About"}> About</RouterLink>
+          <RouterLink to={"/About"}>
+            <Flex align={"center"} gap={"2"}>
+              <Info size={"1rem"} />
+              About
+            </Flex>
+          </RouterLink>
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
